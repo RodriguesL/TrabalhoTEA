a8d52185e627f450398767170a3bbc8b06a5548f
Lucas Rodrigues <rodriguesl@dcc.ufrj.br>
2016-11-23 11:24:17 -0200

    Teste em relação à localização das variáveis (ainda dão 0 por algum motivo)

--- trab.cu
+++ trab.cu
@@ -23,8 +23,8 @@
 int N1;
 int N2;
 
-double h1 = 1/(N1 + 1);
-double h2 = 1/(N2 + 1);
+double h1;
+double h2;
 
 double a(double x, double y) {
     return ITER*x*(1 - x)*(0.5 - y);
@@ -34,15 +34,15 @@ double b(double x, double y) {
     return ITER*y*(1-y)*(x - 0.5);
 }
 double o (int i, int j){
-    return (2 + h1*a(i*h1, j*h2))/4*(1 + (h1*h1/h2*h2));
+    return (2 + h1*a(i*h1, j*h2))/4*(1 + ((h1*h1)/(h2*h2)));
 }
 
 double e (int i, int j) {
-    return (2 - h1*a(i*h1, j*h2))/4*(1 + (h1*h1/h2*h2));
+    return (2 - h1*a(i*h1, j*h2))/4*(1 + ((h1*h1))/(h2*h2));
 }
 
 double s (int i, int j) {
-    return (2 + h2*b(i*h1, j*h2))/4*(1 + (h2*h2/h1*h1));
+    return (2 + h2*b(i*h1, j*h2))/4*(1 + ((h2*h2)/(h1*h1)));
 }
 
 double n (int i, int j) {
@@ -71,10 +71,10 @@ void geraMatriz(double *matriz, int N1, int N2) {
 
 void gauss_seidel_seq(double* atual, int N1, int N2, double w) {
     for (int k = 0; k < ITER; k++) {
-        for (int i = 0; i < N1; i++) {
-            for (int j = 0; j < N2; j++) {
-                    atual[i*N1 + j] = (1 - w)*atual[i*N1 + j] + w*(o(i,j)*atual[(i-1)*N1 + j] +
-                            e(i,j)*atual[(i+1)*N1 + j] + s(i,j)*atual[i*N1 + (j - 1)] + n(i,j)*atual[i*N1 + (j+1)]);
+        for (int i = 1; i < (N1+1); i++) {
+            for (int j = 1; j < (N2+1); j++) {
+                    atual[i*(N1+2) + j] = (1 - w)*atual[i*(N1+2) + j] + w*(o(i,j)*atual[(i-1)*(N1+2) + j] +
+                            e(i,j)*atual[(i+1)*(N1+2) + j] + s(i,j)*atual[i*(N1+2) + (j - 1)] + n(i,j)*atual[i*(N1+2) + (j+1)]);
             }
         }
     }
@@ -86,9 +86,9 @@ void gauss_seidel_local_seq(double *matriz) {
 
 void imprimeMatriz(double *a, int N1, int N2) {
     int i, j;
-    for (i = 0 ; i < N1; i++) {
-        for (j = 0; j < N2; j++) {
-            printf("%.2f ", a[i*N1+j]);
+    for (i = 0 ; i < N1 + 2; i++) {
+        for (j = 0; j < N2 + 2; j++) {
+            printf("%.2lf ", a[i*(N1+2)+j]);
         }
         printf("\n");
     }
@@ -101,10 +101,12 @@ int main (int argc, char** argv) {
     }
     N1 = atoi(argv[1]);
     N2 = atoi(argv[2]);
+    h1 = 1/(N1 + 2);
+    h2 = 1/(N2 + 2);
     double *m;
-    m = (double *) calloc(N1*N2, sizeof(double));
+    m = (double *) calloc((N1+2)*(N2+2), sizeof(double));
     printf("N1 = %d, N2 = %d\n"
-            "h1 = %f, h2 = %f\n", N1, N2, h1, h2);
+            "h1 = %lf, h2 = %lf\n", N1, N2, h1, h2);
     geraMatriz(m, N1, N2);
     imprimeMatriz(m, N1, N2);
     printf("\n\n\n\n");
