1c0f9a342daf3f96250c1fa4d2961abd080ce662
Lucas Rodrigues <rodriguesl@dcc.ufrj.br>
2016-11-23 15:33:18 -0200

    Gauss-Seidel sequencial consertado, implementação inicial do Gauss-Seidel com SORL.

--- trab.cu
+++ trab.cu
@@ -14,11 +14,12 @@
 		exit(EXIT_FAILURE); } }
 */
 #define TAM_BLOCO 32
-#define ITER 500
+#define ITER 1000
 #define UO 0
 #define UE 10
 #define UN 5
 #define US 5
+#define PI 3.141592653589793
 
 int N1;
 int N2;
@@ -26,27 +27,28 @@ int N2;
 double h1;
 double h2;
 
+
 double a(double x, double y) {
-    return ITER*x*(1 - x)*(0.5 - y);
+    return 500*x*(1 - x)*(0.5 - y);
 }
 
 double b(double x, double y) {
-    return ITER*y*(1-y)*(x - 0.5);
+    return 500*y*(1 - y)*(x - 0.5);
 }
 double o (int i, int j){
-    return (2 + h1*a(i*h1, j*h2))/4*(1 + ((h1*h1)/(h2*h2)));
+    return (2 + h1*a(i*h1, j*h2))/(4*(1 + ((h1*h1)/(h2*h2))));
 }
 
 double e (int i, int j) {
-    return (2 - h1*a(i*h1, j*h2))/4*(1 + ((h1*h1))/(h2*h2));
+    return (2 - h1*a(i*h1, j*h2))/(4*(1 + ((h1*h1)/(h2*h2))));
 }
 
 double s (int i, int j) {
-    return (2 + h2*b(i*h1, j*h2))/4*(1 + ((h2*h2)/(h1*h1)));
+    return (2 + h2*b(i*h1, j*h2))/(4*(1 + ((h2*h2)/(h1*h1))));
 }
 
 double n (int i, int j) {
-    return (2 - h2*b(i*h1, j*h2))/4*(1 + (h2*h2/h1*h1));
+    return (2 - h2*b(i*h1, j*h2))/(4*(1 + ((h2*h2)/(h1*h1))));
 }
 
 void geraMatriz(double *matriz, int N1, int N2) {
@@ -64,31 +66,40 @@ void geraMatriz(double *matriz, int N1, int N2) {
     //Resto do chute inicial vai ser o proprio lixo de memoria? Ainda pensar sobre...
     for (int i=1;i<N1-1;i++) {
         for(int j=1;j<N2-1;j++) {
-            matriz[i*N1 + j] = (UO+UE+US+UN)/4;
+            matriz[i*N1 + j] = (double)(UO+UE+US+UN)/4.0;
         }
     }
 }
 
 void gauss_seidel_seq(double* atual, int N1, int N2, double w) {
     for (int k = 0; k < ITER; k++) {
-        for (int i = 1; i < (N1+1); i++) {
-            for (int j = 1; j < (N2+1); j++) {
-                    atual[i*(N1+2) + j] = (1 - w)*atual[i*(N1+2) + j] + w*(o(i,j)*atual[(i-1)*(N1+2) + j] +
-                            e(i,j)*atual[(i+1)*(N1+2) + j] + s(i,j)*atual[i*(N1+2) + (j - 1)] + n(i,j)*atual[i*(N1+2) + (j+1)]);
+        for (int i = 1; i < (N1-1); i++) {
+            for (int j = 1; j < (N2-1); j++) {
+                    atual[i*N1 + j] = (1 - w)*atual[i*N1 + j] + w*(o(i,j)*atual[(i-1)*N1 + j] +
+                            e(i,j)*atual[(i+1)*N1 + j] + s(i,j)*atual[i*N1 + (j - 1)] + n(i,j)*atual[i*N1 + (j+1)]);
             }
         }
     }
 }
 
-void gauss_seidel_local_seq(double *matriz) {
-
+void gauss_seidel_local_seq(double *atual, int N1, int N2) {
+    for (int k = 0; k < ITER; k++) {
+        for (int i = 1; i < (N1 - 1); i++) {
+            for (int j = 1; j < (N2 - 1); j++) {
+                double ro = 2*(sqrt(e(i,j)*o(i,j))*cos(h1*PI)+sqrt(s(i,j)*n(i,j))*cos(h2*PI));
+                double omega = 2/(1 + sqrt(1 - ro*ro));
+                atual[i*N1 + j] = (1 - omega)*atual[i*N1 + j] + omega*(o(i,j)*atual[(i - 1)*N1 + j]+ e(i,j)*atual[(i+1)*N1 + j] +
+                n(i,j)*atual[i*N1 + (j + 1)] + s(i,j)*atual[i*N1 + (j - 1)]);
+            }
+        }
+    }
 }
 
 void imprimeMatriz(double *a, int N1, int N2) {
     int i, j;
-    for (i = 0 ; i < N1 + 2; i++) {
-        for (j = 0; j < N2 + 2; j++) {
-            printf("%.2lf ", a[i*(N1+2)+j]);
+    for (i = 0 ; i < N1; i++) {
+        for (j = 0; j < N2; j++) {
+            printf("%.2lf ", a[i*N1+j]);
         }
         printf("\n");
     }
@@ -101,16 +112,20 @@ int main (int argc, char** argv) {
     }
     N1 = atoi(argv[1]);
     N2 = atoi(argv[2]);
-    h1 = 1.0/(N1 + 2);
-    h2 = 1.0/(N2 + 2);
+    h1 = 1.0/(N1 - 1);
+    h2 = 1.0/(N2 - 1);
     double *m;
-    m = (double *) malloc((N1+2)*(N2+2)*sizeof(double));
+    m = (double *) malloc((N1)*(N2)*sizeof(double));
     printf("N1 = %d, N2 = %d\n"
             "h1 = %lf, h2 = %lf\n", N1, N2, h1, h2);
     geraMatriz(m, N1, N2);
     imprimeMatriz(m, N1, N2);
     printf("\n\n\n\n");
     gauss_seidel_seq(m, N1, N2, 1);
+    imprimeMatriz(m, N1, N2);
+    printf("\n\n\n\n");
+    memset(m, '\0', N1*N2* sizeof(double));
+    gauss_seidel_local_seq(m, N1, N2);
     imprimeMatriz(m, N1, N2);
 
     return 0;
